d508ac3943e1a32c2210d8617ee32a4df57482a4
Sacha Herszkorn <herszkorn.sacha@gmail.com>
2020-11-28 01:23:14 +0100

    [DEL] COVERAGE

--- jest.config.js
+++ jest.config.js
@@ -14,9 +14,6 @@ module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['src/app/**/*.js'],
 
